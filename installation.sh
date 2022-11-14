13b338378b6f225623a48ee6f83d1ddb60d931b4
Alex Sanchez Blabia <alexsanc@car13s1.42barcelona.com>
2022-11-14 10:58:15 +0100

    Added python to path

--- installation.sh
+++ installation.sh
@@ -100,7 +100,6 @@ fi
 echo "Francinette installation Complete. Proceeding to the next step."
 
 # Moulinette installation
-// Add python to PATH
 
 echo "export PATH="$HOME/.local/bin:$PATH" >> ~/.profile
 python3 -m pip install --upgrade pip setuptools
